2d4ebee183fad8409ed64b892f8c1e7d69889f55
Eddy Chang <eddy@joomla.com.tw>
2019-10-28 09:07:17 +0800

    Update CounterApp.js

--- fetch-api/counterapp/src/component2/CounterApp.js
+++ fetch-api/counterapp/src/component2/CounterApp.js
@@ -12,6 +12,7 @@ class CounterApp extends React.Component {
   // 元件 "已經 Did" 呈現在網頁上
   async componentDidMount() {
     try {
+      // 目前沒有使用
       //await this.setState({ loading: true })
 
       const response = await fetch('http://localhost:5555/counter/1', {
